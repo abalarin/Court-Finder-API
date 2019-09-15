606087d13876a971c98865b0eb6e3d4d32d7d3fd
abalarin <abalarin@outlook.com>
2019-09-15 16:11:17 -0400

    adds post validation

--- courtfinderapi/endpoints/users.py
+++ courtfinderapi/endpoints/users.py
@@ -13,14 +13,15 @@ CORS(users) # enable CORS on the courts blue print
 
 @users.route('/login', methods=['POST'])
 def login():
-    username = request.json['user']['username']
-    password_candidate = request.json['user']['password']
-
-    if username is None or password_candidate is None:
+    
+    if(request.json):
+        username = request.json['user']['username']
+        password_candidate = request.json['user']['password']
+    else:
+    # if username is None or password_candidate is None:
         return {"error": "Plz info"}
 
     result = session.query(User).filter_by(username=username).first()
-    print(result)
 
     # If a user exsists and passwords match - login
     if result is not None and sha256_crypt.verify(password_candidate, result.password):
